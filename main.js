2d52dcc9a0cf853f0b243c883edcad29fe615a1c
Neel <2019nupatel@gmail.com>
2025-06-12 10:13:40 -0500

    working on cursor

---
+++ main.js
@@ -0,0 +1,25 @@
+/*
+let shooting=document.querySelector(".shooting-stars");
+let i=0;
+let x=10
+let y=20
+shooting.style.top = x + "px";
+shooting.style.left = y + "px";
+function shootingStar(){
+    if (i<3000){
+        i++;
+        x+=.02
+        y+=.3
+        shooting.style.top = x + "px";
+        shooting.style.left = y + "px";
+    }
+    else{
+        i=0;
+        x=10
+        y=20
+        shooting.style.top = x + "px";
+        shooting.style.left= y + "px";
+    }
+}
+setInterval(shootingStar, 5)
+*/
